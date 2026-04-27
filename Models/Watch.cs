e0c42016122b3f8929f3f68199c4cbe05b90d3fb
HarinDulneth <harindulneth@gmail.com>
2026-04-27 20:39:55 +0530

    first commit

---
+++ Models/Watch.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DialDesk.Server.Models
+{
+    public class Watch
+    {
+        [Key]
+        public string sku_id { get; set; }
+
+        public string model_id {get; set; }
+
+        public string? serial_no { get; set; }
+
+        public string? color { get; set; }
+
+        public string? strap_metrial { get; set; }
+
+        public DateTime recieved_at { get; set; }
+
+        public DateTime Updated_at { get; set; }
+    }
+}
