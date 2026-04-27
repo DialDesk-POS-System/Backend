24146bd0c73fd01c473a8bb2f5c1dca689e8c2cd
HarinDulneth <harindulneth@gmail.com>
2026-04-27 22:01:13 +0530

    Add mdoels

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,6 +1,35 @@
-﻿namespace DialDesk.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DialDesk.Server.Models
 {
     public class Model
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ModelNo { get; set; }
+
+        [Required]
+        public Category category { get; set; }
+
+        public int BrandId { get; set; }
+        [ForeignKey("BrandId")]
+        public required Brand Brand { get; set; }
+
+        [Required]
+        public string ModelName { get; set; }
+
+        [Required]
+        public decimal BasePrice { get; set; }
+
+        [Required]
+        public int WarrantyPeriod { get; set; }
+
+        public Boolean IsActive { get; set; }
+
+        [InverseProperty("Model")]
+        public List<Watch> Watches { get; set; } = [];
     }
 }
