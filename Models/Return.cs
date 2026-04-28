18b9dfc066a4bf25b89843bcfd1b6ab12a3dad21
sanjanadissa <sddsdonz@gmail.com>
2026-04-28 18:19:37 +0530

    add Return model

---
+++ Models/Return.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DialDesk.Server.Models
+{
+    public class Return
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OriginalSaleItemId { get; set; }
+
+        [ForeignKey("OriginalSaleItemId")]
+        public SaleItem OriginalSaleItem { get; set; }
+
+        [Required]
+        public int NewSaleItemId { get; set; }
+        [ForeignKey("NewSaleItemId")]
+        public SaleItem? NewSaleItem { get; set; }
+
+        [Required]
+        public decimal RefundAmount { get; set; }
+
+        [Required]
+        public DateTime ReturnDate { get; set; }
+
+
+
+    }
+}
